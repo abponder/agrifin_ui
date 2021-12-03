889653a63c4932d862735fa14098ebc73c191c5d
alexbponder@gmail.com <alexbponder@gmail.com>
2021-12-02 17:36:13 -0800

    added axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+const axios = require('axios');
+
 
 function App() {
+  const [data, setData] = useState([]); // js destructuring; useState is react
+  //useEffect ... if you want your data before you render the component
+  useEffect(()=>{ 
+    axios.get('/api')
+    .then(function (response) {
+    setData(response.data)
+    console.log(response);
+  })
+    .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+    .then(function () {
+    // always executed
+  });
+ },
+  [] )
+ 
   return (
     <div>
       Hello World
+      {/* if data length is greater than 0 same as truthy, then return second part of statement */}
+      {data.length && data.map(obj=>obj.ID)}
     </div>
+
   );
 }
 
